c4952c72de76dd8b623161050640ecd6a9ac20ee
Turkeyo <Zxc488451726@gmail.com>
2022-06-30 11:54:17 +0800

    Add code

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def handle_message(event):
 
 if __name__ == "__main__":
     app.run()
-    port = int(os.environ.get('PORT',56913))
+    port = int(os.environ.get('PORT',5000))
     app.run(host='127.0.0.1',port=port,debug=True)
