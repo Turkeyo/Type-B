e5cccf4d6f206491f26965e13e16c0908f120006
Turkeyo <Zxc488451726@gmail.com>
2022-06-30 08:47:48 +0800

    Add code

--- app.py
+++ app.py
@@ -33,12 +33,15 @@ def callback():
 
     return 'OK'
 
-
+#機器人回覆訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    #line_bot_api.reply_message(
+    #    event.reply_token,
+    #    TextSendMessage(text=event.message.text))
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text="Hello World"))
 
 
 if __name__ == "__main__":
