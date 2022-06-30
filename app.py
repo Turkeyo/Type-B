51b8cf19c52d4bda733a370481d1c86beb29d693
Turkeyo <Zxc488451726@gmail.com>
2022-06-30 08:50:51 +0800

    Add code

--- app.py
+++ app.py
@@ -39,9 +39,11 @@ def handle_message(event):
     #line_bot_api.reply_message(
     #    event.reply_token,
     #    TextSendMessage(text=event.message.text))
+    message = TextSendMessage(text='Hello World')
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="Hello World"))
+        TextSendMessage(event.reply_token, message))
 
 
 if __name__ == "__main__":
