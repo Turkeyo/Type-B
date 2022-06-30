ac85fa9171ab2b2795c2efb24c7a4c8d1ca9c01d
Turkeyo <Zxc488451726@gmail.com>
2022-06-30 13:51:19 +0800

    LineBot

--- main.py
+++ main.py
@@ -34,16 +34,11 @@ def callback():
     return 'OK'
 
 #機器人回覆訊息
-#@handler.add(MessageEvent, message=TextMessage)
-#def handle_message(event):
-#    line_bot_api.reply_message(
-#        event.reply_token,
-#        TextSendMessage(text=event.message.text))
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
     app.run()
