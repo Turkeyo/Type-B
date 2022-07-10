477b193eea96142ce2358c5ed49d015577a5ece4
Turkeyo <Zxc488451726@gmail.com>
2022-07-10 09:38:39 +0800

    LineBot

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def reply(event,message):
         )
         line_bot_api.reply_message(event.reply_token, buttons_template_message)
     if(re.match("柴犬抽1張",message)):
-        imgTitle,imgSrc = ""
+        imgTitle = ""
         imgTitle = randomChoice.cho.choicetitle(imgTitle)
         #imgSrc = randomChoice.cho.choicesrc(imgSrc)
         #image_message = ImageSendMessage(
